9f7cbbb6cffab249b8222b117d60f187fe21d954
yunlong <laureler@163.com>
2019-01-13 12:35:37 +0800

    修改路由内部导航钩子函数的位置

--- src/views/TypeScriptDemo.vue
+++ src/views/TypeScriptDemo.vue
@@ -13,5 +13,26 @@ import TSInVue from '@/components/TSInVue.vue' // @ is an alias to /src
 		TSInVue
 	}
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+	// 路由钩子函数
+	// The class component now treats beforeRouteEnter
+	// and beforeRouteLeave as Vue Router hooks
+	beforeRouteEnter (to, from, next) {
+		console.group('%c%s', 'background-color:white', `组件内部路由守卫钩子函数：beforeRouterEnter`)
+		console.log('%c%s', 'background-color:#6e825e', 'beforeRouterEnter')
+		next() // needs to be called to confirm the navigation
+	}
+
+	beforeRouteLeave (to, from, next) {
+		console.group('%c%s', 'background-color:white', `组件内部路由守卫钩子函数：beforeRouteLeave`)
+		console.log('%c%s', 'background-color:#6e825e', 'beforeRouteLeave')
+		next() // needs to be called to confirm the navigation
+		console.groupEnd()
+	}
+	beforeRouteUpdate (to, from, next) {
+		console.group('%c%s', 'background-color:white', `组件内部路由守卫钩子函数：beforeRouteUpdate`)
+		console.log('%c%s', 'background-color:#6e825e66', 'beforeRouteUpdate')
+		next()
+	}
+}
 </script>
