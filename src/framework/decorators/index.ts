0e8351d2f43de7b529942c114721004436aae936
yunlong <laureler@163.com>
2019-01-13 09:40:58 +0800

    避开TS类型限制

--- src/framework/decorators/index.ts
+++ src/framework/decorators/index.ts
@@ -1,10 +1,13 @@
 import { createDecorator } from 'vue-class-component'
-import {ComponentOptions,Vue} from "vue";
+import { ComponentOptions } from 'vue';
 
 // 创建自定义的 装饰器
-export const NoCache = createDecorator((options:ComponentOptions<Vue>, key) => {
+export const NoCache = createDecorator((options, key) => {
 	// component options should be passed to the callback
 	// 组件的options选 应该传递给回调方法
 	// 选项的更新也会进而影响到组件
-	options.computed[key].cache = false
+
+	var computed:any = options.computed;            // 设置any避开TS的类型限制
+	var computedElement:any = computed[key];        // 设置any避开TS的类型限制
+	computedElement.cache = false
 })
