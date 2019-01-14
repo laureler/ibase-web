61923b76c55035ac23cb24a8bc83bfbdb7970b6f
yunlong <laureler@163.com>
2019-01-14 19:42:17 +0800

    重构 *.d.ts位置

--- src/templateComponent/layout.vue
+++ src/templateComponent/layout.vue
@@ -1,15 +1,20 @@
 <template>
 	<div class="hello">
-		layout 布局
-		{{msg}}
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link> |
+			<router-link to="/TSInVue">TSInVue</router-link> |
+			<router-link to="/AxiosInVue">AxiosInVue</router-link> |
+			<router-link to="/about">About</router-link> |
+		</div>
 		<div>
 			<div>
 				<sg-button>默认按钮</sg-button>
-				<sg-button type="primary">主按钮</sg-button>
-				<sg-button type="info">基本按钮</sg-button>
-				<sg-button type="success">成功按钮</sg-button>
-				<sg-button type="warning">警告按钮</sg-button>
-				<sg-button type="error">错误按钮</sg-button>
+				<sg-button type="primary">主按钮</sg-button>&nbsp;
+				<sg-button type="info">基本按钮</sg-button>&nbsp;
+				<sg-button type="success">成功按钮</sg-button>&nbsp;
+				<sg-button type="warning">警告按钮</sg-button>&nbsp;
+				<sg-button type="error">错误按钮</sg-button>&nbsp;
 			</div>
 			<div style="margin-top: 15px;">
 				<sg-button type="text">文本按钮</sg-button>
