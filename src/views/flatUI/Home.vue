caf5f9609533077786d17f4b04de5f3b3bf8a898
yunlong <laureler@163.com>
2019-01-15 14:31:19 +0800

    提交不同界面版本

---
+++ src/views/flatUI/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/TSInVue">TSInVue</router-link> |
+      <router-link to="/AxiosInVue">AxiosInVue</router-link> |
+      <router-link to="/about">About</router-link> |
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+// @ is an alias to /src
+import Layout from '@/template/layout.vue'
+
+
+@Component({
+	components: {
+		Layout
+	}
+})
+export default class Home extends Vue {}
+</script>
---
+++ src/views/flatUI/HomeBak.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+	@Component({
+		components: {
+			HelloWorld
+		}
+	})
+export default class Home extends Vue {
+}
+</script>
